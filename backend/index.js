075da76608373725aa25ebfcd20ffcc5204606bd
Sankalp Naranje <sankalpnaranje@gmail.com>
2024-06-28 10:41:53 +0530

    Vercel_origin_added

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port = 5000
 //setting up cors
 app.use(cors(
   {
-    origin: ['http://localhost:3000'],
+    origin: ['https://self-book-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }
